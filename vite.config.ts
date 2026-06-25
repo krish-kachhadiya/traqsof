0ccb550a44f00eedd0860ac51006fd34b260ac95
krish-kachhadiya <kachhadiyak26@gmail.com>
2026-06-25 11:15:05 +0530

    Force enable Nitro for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -12,6 +12,7 @@ export default defineConfig({
     // nitro/vite builds from this
     server: { entry: "server" },
   },
+  nitro: true, // <-- THIS IS THE MAGIC LINE TO FIX VERCEL
   vite: {
     publicDir: "public",
   },
